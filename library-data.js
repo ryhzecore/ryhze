a4f296da3d1317a5ce8a33937f2fcdf6e02ce251
ryhzecore <ryhzeentertainment@gmail.com>
2026-08-31 22:05:13 +0800

    Auto-sync Ryhze updates

--- library-data.js
+++ library-data.js
@@ -135,9 +135,9 @@
                                                                                  "title":  "Episode 4",
                                                                                  "streams":  [
                                                                                                  {
-                                                                                                     "url":  "Films/Loki/Streams/Season%202/Episode%204/Stream1.mp4",
-                                                                                                     "publicUrl":  "https://video.ryhze.com/Films/Loki/Streams/Season%202/Episode%204/Stream1.mp4",
-                                                                                                     "backupPublicUrl":  "https://pub-7e02d448a41c4dbabd5711a9c5f799f0.r2.dev/Films/Loki/Streams/Season%202/Episode%204/Stream1.mp4",
+                                                                                                     "url":  "Films/Loki/Streams/Season%202/Episode%204/Stream1-web.mp4",
+                                                                                                     "publicUrl":  "https://video.ryhze.com/Films/Loki/Streams/Season%202/Episode%204/Stream1-web.mp4",
+                                                                                                     "backupPublicUrl":  "https://pub-7e02d448a41c4dbabd5711a9c5f799f0.r2.dev/Films/Loki/Streams/Season%202/Episode%204/Stream1-web.mp4",
                                                                                                      "type":  ".mp4"
                                                                                                  }
                                                                                              ]
@@ -315,9 +315,9 @@
                                                                                  "title":  "Episode 4",
                                                                                  "streams":  [
                                                                                                  {
-                                                                                                     "url":  "Films/Loki/Streams/Season%202/Episode%204/Stream1.mp4",
-                                                                                                     "publicUrl":  "https://video.ryhze.com/Films/Loki/Streams/Season%202/Episode%204/Stream1.mp4",
-                                                                                                     "backupPublicUrl":  "https://pub-7e02d448a41c4dbabd5711a9c5f799f0.r2.dev/Films/Loki/Streams/Season%202/Episode%204/Stream1.mp4",
+                                                                                                     "url":  "Films/Loki/Streams/Season%202/Episode%204/Stream1-web.mp4",
+                                                                                                     "publicUrl":  "https://video.ryhze.com/Films/Loki/Streams/Season%202/Episode%204/Stream1-web.mp4",
+                                                                                                     "backupPublicUrl":  "https://pub-7e02d448a41c4dbabd5711a9c5f799f0.r2.dev/Films/Loki/Streams/Season%202/Episode%204/Stream1-web.mp4",
                                                                                                      "type":  ".mp4"
                                                                                                  }
                                                                                              ]
@@ -434,9 +434,9 @@
                                                                                  "title":  "Episode 4",
                                                                                  "streams":  [
                                                                                                  {
-                                                                                                     "url":  "Films/Loki/Streams/Season%202/Episode%204/Stream1.mp4",
-                                                                                                     "publicUrl":  "https://video.ryhze.com/Films/Loki/Streams/Season%202/Episode%204/Stream1.mp4",
-                                                                                                     "backupPublicUrl":  "https://pub-7e02d448a41c4dbabd5711a9c5f799f0.r2.dev/Films/Loki/Streams/Season%202/Episode%204/Stream1.mp4",
+                                                                                                     "url":  "Films/Loki/Streams/Season%202/Episode%204/Stream1-web.mp4",
+                                                                                                     "publicUrl":  "https://video.ryhze.com/Films/Loki/Streams/Season%202/Episode%204/Stream1-web.mp4",
+                                                                                                     "backupPublicUrl":  "https://pub-7e02d448a41c4dbabd5711a9c5f799f0.r2.dev/Films/Loki/Streams/Season%202/Episode%204/Stream1-web.mp4",
                                                                                                      "type":  ".mp4"
                                                                                                  }
                                                                                              ]
@@ -567,5 +567,5 @@
                   }
               ]
 };
-window.RyhzeBuild = '13da6c2';
+window.RyhzeBuild = 'fd1c0d2';
 
